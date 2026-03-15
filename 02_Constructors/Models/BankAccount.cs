8f25d0d8770c6bbed1f0602f229584dd2f867864
LuisdelaG21 <luisdxgauss2164@gmail.com>
2026-03-15 14:39:31 -0600

    Nuevo proyecto

---
+++ 02_Constructors/Models/BankAccount.cs
@@ -0,0 +1,15 @@
+using System.Runtime.ConstrainedExecution;
+using System.Security.AccessControl;
+
+public class BankAccount
+{
+    public string Owner { get; set; } = ""; //Necesaria la inicialización con ""
+    public decimal Amount { get; set; }
+    public bool IsActive { get; set; }
+
+    public BankAccount()
+    {
+        Owner = "Fernando Ventura";
+    }
+
+}
---
+++ 02_Constructors/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+BankAccount savingAccount = new();
+
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Hello, World!");
